ebb373dd93a61900c804ee3a3490d76ddbfbb942
sai varma <saivarmaakireddy270@gmail.com>
2026-08-05 17:45:30 +0530

    string methods

---
+++ day01/string methods.py
@@ -0,0 +1,16 @@
+#s='raj'
+#print(len(s))
+#operations on string
+s="raju"
+d="rani"
+results=s+d
+print(results) # cancatination 
+print(results*3)#repetition
+print(len(s))
+print(s[3])#indexing
+print(s[-1])
+print(s[0:1])#slicing
+print(s[0:-1:-1])
+print(s[2:4:1]) 
+print("raju" in s)#membership
+print("ramu" in s)
